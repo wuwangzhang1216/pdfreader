024cb02ea2c5bdb4866fa11e87eb811ed5a9ae27
SteveWu <60851473+wuwangzhang1216@users.noreply.github.com>
2024-07-22 22:04:37 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# pdfreader
-local GPT chat with pdf
+# GPT-PDFReader
+
+Use the ChatGPT API to answer questions about content in a PDF file without using OpenAI embeddings API.
+
+## Usage
+
+```console
+$ ./conversation.py [PDF_FILE] [CHUNK_SIZE] [OVERLAP] [LIMIT] [MODEL]
+```
+
+All command line arguments are optional. You can `export OPENAI_API_KEY=[YOUR_API_KEY]` if you don't want to input it every time.
+
+## Requirements
+
+```console
+$ pip install openai pypdf2
+```
---
+++ conversation.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import openai
+import sys
+import os
+
+import pdf_reader
+import lookup
+import gpt
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+if not openai.api_key:
+    openai.api_key = input("Please enter your OpenAI API key: ")
+    print()
+
+program_name = sys.argv.pop(0)
+
+if len(sys.argv):
+    pdf_file = sys.argv.pop(0)
+else:
+    pdf_file = input("Please enter the PDF file you want to read: ")
+    print()
+
+    while not os.path.exists(pdf_file):
+        print("ERROR: File not found")
+        pdf_file = input("Please enter the PDF file you want to read: ")
+        print()
+
+if len(sys.argv):
+    chunk_size = int(sys.argv.pop(0))
+else:
+    chunk_size = 4000
+
+if len(sys.argv):
+    overlap = int(sys.argv.pop(0))
+else:
+    overlap = 1000
+
+if len(sys.argv):
+    limit = int(sys.argv.pop(0))
+else:
+    limit = 5
+
+if len(sys.argv):
+    gpt.model = sys.argv.pop(0)
+else:
+    gpt.model = "gpt-4o"
+
+print("Chunking PDF...\n")
+chunks = pdf_reader.chunk_pdf(pdf_file, chunk_size, overlap)
+
+while True:
+    question = input("GPT: What do you want to know?\nYou: ")
+    print()
+    keywords = gpt.get_keywords(question)
+
+    print("Searching: " + ", ".join(keywords) + "")
+
+    matches = lookup.find_matches(chunks, keywords)
+
+    for i, chunk_id in enumerate(matches.keys()):
+        print(".", end="", flush=True)
+
+        chunk = chunks[chunk_id]
+        response = gpt.answer_question(chunk, question)
+
+        if response.get("answer_found"):
+            print("\n\nGPT: " + str(response.get("response")) + "\n")
+            break
+
+        if i > limit:
+            break
+
+    if not response.get("answer_found"):
+        print("\n\nGPT: I'm sorry, but I can't find that information\n")
